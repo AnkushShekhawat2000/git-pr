133d4a8f3fcf218902b338e1be2dd0e6b70d804d
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2025-02-19 11:35:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,20 +4,5 @@ import Dashboard from "./components/Home/dashboard"
 import Login from "./components/Home/login"
 import ProtectedRouted from "./components/ProtectedRoute";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ProtectedRouted> <Dashboard /></ProtectedRouted>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={
-           <ProtectedRouted>
-                            <Dashboard />
-          </ProtectedRouted>
-        } />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
 export default App;
